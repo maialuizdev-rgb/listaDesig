e5dc7e072a6ca24f338e270e8d6b3f81fd05d0d6
maialuizdev-rgb <maia.luiz.dev@gmail.com>
2025-09-05 18:32:52 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/listaDesig',
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser'
+  }
 })
